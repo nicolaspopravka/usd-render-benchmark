cc22a590d380bfdeb1d74859824df0304ddb29bf
Nicolas Popravka <nicolaspopravka@gmail.com>
2026-07-28 12:53:25 +0200

    Publish ASWF CY2025 Storm benchmark

---
+++ packages/usd/25.05/package.py
@@ -0,0 +1,14 @@
+name = "usd"
+
+version = "25.05"
+
+requires = []
+
+
+def commands():
+    path = "/usr/local"
+
+    alias("usdrecord", "/workspace/usd-render-benchmark/tools/usdrecord_egl.py")
+
+    env.PYTHONPATH.append(path + "/lib/python")
+    env.PATH.append(path + "/bin")
