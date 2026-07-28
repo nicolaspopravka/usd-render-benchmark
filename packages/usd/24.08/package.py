8daa2eeee2d9e2d969dc26472e5dfef4cf0fbe98
Nicolas Popravka <nicolaspopravka@gmail.com>
2026-07-28 11:17:21 +0200

    Publish ASWF CY2024 GL benchmark

--- packages/usd/24.08/package.py
+++ packages/usd/24.08/package.py
@@ -2,31 +2,13 @@ name = "usd"
 
 version = "24.08"
 
-variants = [
-    ['prman-26.1', 'embree-3.3.2']
-]
+requires = []
+
 
 def commands():
-    env.PYTHONPATH.append("/opt/pixar/USD-24.08/lib/python")
-    env.PATH.append("/opt/pixar/USD-24.08/bin")
+    path = "/usr/local"
 
-    # Running hdPrman
-    # See https://openusd.org/docs/RenderMan-USD-Imaging-Plugin.html
+    alias("usdrecord", "/workspace/usd-render-benchmark/tools/usdrecord_egl.py")
 
-    path = "/opt/pixar/RenderManProServer-26.1"
-    usd_path = "/opt/pixar/USD-24.08"
-
-    # Directory where Pixar's RenderMan is installed
-    env.RMANTREE.append(path)
- 
-    # Search path for RenderMan OSL shader plugins
-    env.RMAN_SHADERPATH.append(path + "/lib/shaders")
-    env.RMAN_SHADERPATH.append(usd_path + "/plugin/usd/resources/shaders")
-
-    # Search path for RenderMan RIS shading plugins
-    env.RMAN_RIXPLUGINPATH.append(path + "/lib/plugins")
-
-    # Search path for textures and RenderMan Rtx plugins
-    env.RMAN_TEXTUREPATH.append(path + "/lib/textures")
-    env.RMAN_TEXTUREPATH.append(path + "/lib/plugins")
-    env.RMAN_TEXTUREPATH.append(usd_path + "/plugin/usd")
+    env.PYTHONPATH.append(path + "/lib/python")
+    env.PATH.append(path + "/bin")
--- render_script.sh
+++ render_script.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-# Define arrays for renderers and their corresponding packages
 declare -A renderers
 renderers=(
-    ["GL"]="usd"                                # 4.6.0 (24.08)
-    ["Karma CPU"]="houdini"                     # 20.0.751 (23.08)
-    ["Karma XPU"]="houdini"
-    ["Prman"]="usd"                             # 26.1 (24.08)
-    ["RenderMan RIS"]="rfh"                     # 26.1 (23.08)
-    ["RenderMan XPU - CPU"]="rfh"
-    ["RenderMan XPU - GPU"]="rfh"
-    ["RenderMan XPU"]="rfh"
-    ["Arnold"]="htoa"                           # 7.3.3.0 (23.08)
-    ["Moonray"]="moonray"                       # 1.5 (22.05)
-    ["Cycles"]="cycles"                         # 3.6.0 (23.08)
-    ["Embree"]="usd"                            # 3.2.2 (24.08)
+    ["GL"]="usd"                                # 24.08
 )
 
 # Define arrays for scenes, cameras and optional frame specifications
@@ -28,22 +16,19 @@ scenes_and_cameras=(
 # Define problematic combinations of renderer and scene
 declare -A problematic_combinations
 problematic_combinations=(
-    ["Arnold,scenes/MoanaIsland/usd/island.usda"]=1 # 00:13:16 65579MB WARNING |  signal caught: SIGSEGV -- Invalid memory reference
-    ["Moonray,scenes/MoanaIsland/usd/island.usda"]=1 # Segmentation fault (core dumped)
-    ["Cycles,scenes/ALab/ALab/entry.usda"]=1 # 49784 ThreadId=0x7f34597fe700: Fatal error: Segmentation fault
-    ["Cycles,scenes/MoanaIsland/usd/island.usda"]=1 # 48444 ThreadId=0x7f78c4b52700: Fatal error: Segmentation fault
+    ["GL,scenes/MoanaIsland/usd/island.usda"]=1 # Segmentation fault (core dumped)
 )
 
 # Loop over each renderer
 for renderer in "${!renderers[@]}"; do
     package=${renderers[$renderer]}
-    
+
     # Loop over each scene, camera and frame specifications
     for scene_and_camera in "${scenes_and_cameras[@]}"; do
         scene=$(echo $scene_and_camera | awk '{print $1}')
         camera=$(echo $scene_and_camera | awk '{print $2}')
         frames=$(echo $scene_and_camera | awk '{print $3}')
-        
+
         # Check if this combination is problematic
         combination="${renderer},${scene}"
         if [[ -n "${problematic_combinations[$combination]}" ]]; then
@@ -52,7 +37,7 @@ for renderer in "${!renderers[@]}"; do
         fi
 
         scene_name=$(basename "$scene" | sed 's/\.[^.]*$//')  # Get the scene name without extension
-        
+
         # Determine frames option and modify output path if needed
         if [ -z "$frames" ]; then
             frames_option=""
@@ -62,14 +47,14 @@ for renderer in "${!renderers[@]}"; do
             output_path="renderers/${renderer}/${scene_name}.#.jpg"
         fi
 
-	# Ensure the output directory exists
-	output_dir=$(dirname "$output_path")
-	mkdir -p "$output_dir"
+        # Ensure the output directory exists
+        output_dir=$(dirname "$output_path")
+        mkdir -p "$output_dir"
 
         # Create a log file for this run
         log_file="logs/${renderer}_${scene_name}.log"
-	mkdir -p logs  # Ensure the logs directory exists
-        
+        mkdir -p logs  # Ensure the logs directory exists
+
         # Run the command and log the output
         echo "Running: rez env $package -- usdrecord --camera $camera --renderer \"$renderer\" --purposes render  $frames_option $scene \"$output_path\"" | tee "$log_file"
         (/usr/bin/time -f "Time: %E\nMemory: %M KB" rez env $package -- usdrecord --camera $camera --renderer "$renderer" --purposes render $frames_option $scene "$output_path" || echo "ERROR: usdrecord returned non-zero exit status") |& tee -a "$log_file"
