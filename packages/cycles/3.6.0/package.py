f6d2b5ec8c13add663a0e95219c1c9d81328de29
Nicolas Popravka <nicolaspopravka@gmail.com>
2026-08-08 18:23:24 +0200

    Publish AWSF CY2023 Cycles benchmark

--- packages/cycles/3.6.0/package.py
+++ packages/cycles/3.6.0/package.py
@@ -3,8 +3,11 @@ name = "cycles"
 version = "3.6.0"
 
 variants = [
-    ["houdini-20.0.751"]
+    ["usd-23.08"]
 ]
 
 def commands():
-    env.PXR_PLUGINPATH_NAME.append("/opt/cycles/houdini/dso/usd_plugins")
+    path = "/opt/cycles"
+
+    env.LD_LIBRARY_PATH.prepend(path + "/lib")
+    env.PXR_PLUGINPATH_NAME.prepend(path + "/hydra")
---
+++ packages/usd/23.08/package.py
@@ -0,0 +1,14 @@
+name = "usd"
+
+version = "23.08"
+
+requires = []
+
+
+def commands():
+    path = "/opt/usd"
+
+    alias("usdrecord", "/workspace/usd-render-benchmark/tools/usdrecord_egl.py")
+
+    env.PYTHONPATH.append(path + "/lib/python")
+    env.PATH.append(path + "/bin")
