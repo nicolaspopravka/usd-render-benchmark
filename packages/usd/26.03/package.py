ed14a71851e51d6738604e0941606a5d2bf1b442
Nicolas Popravka <nicolaspopravka@gmail.com>
2026-09-02 21:51:50 +0200

    Publish ASWF CY2026 Embree benchmark

--- packages/usd/26.03/package.py
+++ packages/usd/26.03/package.py
@@ -1,9 +1,15 @@
 name = "usd"
 
-version = "22.05"
+version = "26.03"
+
+variants = [
+    ['embree-4.3.3']
+]
 
 def commands():
     path = "/usr/local"
+
+    alias("usdrecord", "/workspace/usd-render-benchmark/tools/usdrecord_egl.py")
 
     env.PYTHONPATH.append(path + "/lib/python")
     env.PATH.append(path + "/bin")
